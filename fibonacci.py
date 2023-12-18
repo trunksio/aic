3442a99655e2fd3ee5fc027313629b942ffdd915
Lewis Crawford <lewisc@Lewiss-MacBook-Air.local>
2023-12-18 14:48:38 +0000

    aider: Create a Python file to print out the Fibonacci sequence up to a specified number.
    # Aider chat conversation:
    
    USER: Create a simple Python file to print out the Fibonacci sequence.
    
    ASSISTANT: Sure, we can create a new Python file named `fibonacci.py` that will contain a function to print out the Fibonacci sequence.
    
    Here is the *SEARCH/REPLACE block* for the new file:
    
    fibonacci.py
    ```python
    <<<<<<< SEARCH
    =======
    def fibonacci(n):
        "print the Fibonacci sequence up to n"
    
        a, b = 0, 1
        while a < n:
            print(a, end=' ')
            a, b = b, a+b
        print()
    
    if __name__ == '__main__':
        fibonacci(1000)
    >>>>>>> REPLACE
    ```
    
    This script will print out the Fibonacci sequence up to 1000 when run. You can modify the argument to the `fibonacci` function call in the `if __name__ == '__main__':` block to print out a different number of terms.

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    "print the Fibonacci sequence up to n"
+
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+
+if __name__ == '__main__':
+    fibonacci(1000)
